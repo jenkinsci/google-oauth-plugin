d17970256eed92bf1a7bfcfc959196ae3e66036f
Jonathan Leitschuh <jonathan.leitschuh@gmail.com>
2023-03-20 21:21:42 -0400

    vuln-fix: Temporary Directory Hijacking or Information Disclosure (#152)
    This fixes either Temporary Directory Hijacking, or Temporary Directory Local Information Disclosure.
    
    Weakness: CWE-379: Creation of Temporary File in Directory with Insecure Permissions
    Severity: High
    CVSSS: 7.3
    Detection: CodeQL & OpenRewrite (https://public.moderne.io/recipes/org.openrewrite.java.security.UseFilesCreateTempDirectory)
    
    Reported-by: Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
    
    
    Bug-tracker: https://github.com/JLLeitschuh/security-research/issues/10
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/com/google/jenkins/plugins/credentials/oauth/P12ServiceAccountConfigTestUtil.java
+++ src/test/java/com/google/jenkins/plugins/credentials/oauth/P12ServiceAccountConfigTestUtil.java
@@ -19,6 +19,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.file.Files;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.KeyStore;
@@ -64,13 +65,7 @@ public class P12ServiceAccountConfigTestUtil {
 
   private static File getTempFolder() throws IOException {
     if (tempFolder == null) {
-      tempFolder = File.createTempFile("temp", Long.toString(System.nanoTime()));
-      if (!tempFolder.delete()) {
-        throw new IOException("Could not delete temp file: " + tempFolder.getAbsolutePath());
-      }
-      if (!tempFolder.mkdir()) {
-        throw new IOException("Could not create temp directory: " + tempFolder.getAbsolutePath());
-      }
+      tempFolder = Files.createTempDirectory("temp" + Long.toString(System.nanoTime())).toFile();
       tempFolder.deleteOnExit();
     }
     return tempFolder;
